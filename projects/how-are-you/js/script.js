e41cd61c22cb6aaa589ec59ac13dac987cb5302f
ehatlebe <github email address>
2019-03-12 06:05:01 -0700

    updated responses

--- projects/how-are-you/js/script.js
+++ projects/how-are-you/js/script.js
@@ -1,52 +1,55 @@
+/* emotion variables of possible responses */
 var happy = [
-  "It\'s time for a 5 minute Michael Jackson dance break.",
+  "It\'s time for a 5-minute dance break.",
   "Rock on!",
-  "You're awesome!",
-  "Smiles are contageous, share one with someone.",
-  "When you're happy and you know it, clap your hands!"
+  "You\'re awesome!",
+  "Smiles are contagious, share one.",
+  "Carry this feeling with you."
 ]
 
 var sad = [
-  "It\'s time for a 5 minute Michael Jackson dance break.",
+  "It\'s time for a 5-minute dance break.",
   "Hug.",
   "I am glad you\'re you.",
   "I am sending you sunshine!",
-  "This too shall pass."
+  "This too shall pass.",
+  "It's ok."
 ];
 
 var defeated = [
-  "It\'s time for a 5 minute Michael Jackson dance break.",
+  "It\'s time for a 5-minute dance break.",
   "Keep going.",
   "Our struggles can be our greatest teachers.",
-  "Real strength comes from picking ourselves up after a fall and continuing to move in the direction of our dreams.",
+  "Continue to move in the direction of our dreams.",
   "I believe in you."
 ];
 
 var lost = [
-  "It\'s time for a 5 minute Michael Jackson dance break.",
-  "Find a way to let your light shine. You have something special to share, and the world will be better for it.",
+  "It\'s time for a 5-minute dance break.",
+  "Find a way to let your light shine.",
   "Maybe it's time for an adventure.",
   "Do something nice for someone else. It helps.",
-  "It is up to us to build a life we love. No one will do it for us."
+  "It is up to us to build a life we love."
 ];
 
 var mad = [
-  "It\'s time for a 5 minute Michael Jackson dance break.",
+  "It\'s time for a 5-minute dance break.",
   "We are the first victim of our anger.",
   "Take a deep breath.",
   "Don't bite the hook, you will regret it.",
-  "What about taking a walk?"
+  "What about taking a walk?",
+  "Step away from the computer."
 ];
 
 var grateful = [
-  "It\'s time for a 5 minute Michael Jackson dance break.",
-  "Who is someone you appreciate? Tell them.",
-  "Everyday is a gift, there is so much to be greatful for.",
+  "It\'s time for a 5-minute dance break.",
+  "Who do you appreciate? Tell them.",
+  "Everyday is a gift.",
   "Live your life with arms wide open.",
   "Gratitude is like a muscle, flex it daily!"
 ];
 
-/* in newResponse, randomNumber generates a valid array index number happyResponse array and then the happyResponse goes into the HTML element newResponse */
+/* functions determine which random response to use depending on the emotion button selected and then the response is displayed in displayResponse */
 function happyResponse() {
   var randomNumber = Math.floor(Math.random() * (happy.length));
   document.getElementById('displayResponse').innerHTML = happy[randomNumber];
